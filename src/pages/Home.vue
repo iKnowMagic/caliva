eb0f975c2c231d5527fbaf9d83f3de23436d5fdf
iknowmagic <ilan@iknowmagic.net>
2018-11-17 15:27:56 -0800

    sample product

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
-    {{cproducts}}
+    <div class="card" style="width: 18rem;">
+  <img class="card-img-top" :src="cproducts[0].photo" :alt="cproducts[0].name">
+  <div class="card-body">
+    <h5 class="card-title">{{cproducts[0].name}}</h5>
+    <p class="card-text">{{cproducts[0].description}}</p>
+  </div>
+</div>
+    {{cproducts[0]}}
   </div>
 </template>
 
@@ -29,7 +36,13 @@ export default {
       console.log(this.products)
       this.cproducts = _.map(this.products, (product) => {
         return {
-          photo: product.photos[0]
+          photo: product.photos[0],
+          id: product._id,
+          category: product.category,
+          description: product.description,
+          name: product.name,
+          price: product.pricing.unit.value,
+          species: product.species
         }
       })
     })
