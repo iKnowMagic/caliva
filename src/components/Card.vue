0f55ace8a34af917d02b1dfcf6ae7f9b1440cac0
iknowmagic <ilan@iknowmagic.net>
2018-11-17 16:44:30 -0800

    sample product

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,19 +1,18 @@
 <template>
-  <div class="card-group">
-    <div class="card">
-      <div class="card-body">
-        <slot />
-      </div>
+  <div class="card" style="width: 18rem">
+    <img class="card-img-top" :src="photo" :alt="name">
+    <div class="card-body">
+      <h5 class="card-title">{{name}}</h5>
+      <!-- <p class="card-text">{{description}}</p> -->
     </div>
   </div>
 </template>
 
 <script>
 export default {
-
+  props: ['photo', 'description', 'name']
 }
 </script>
 
 <style scoped>
-
 </style>
